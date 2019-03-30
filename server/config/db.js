d38839acc8e60ddcc0ed387091827bdf08095390
parupatel <connectparupatel@gmail.com>
2019-03-30 12:57:43 -0400

    commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
 module.exports = {
 //"URI" : "mongodb://localhost/test",
-"URI" : "mongodb://thomas:a123456@ds058739.mlab.com:58739/comp308-w2019-lesson10a",
-"secret": "SomeSecret"
+//"URI" : "mongodb://thomas:a123456@ds058739.mlab.com:58739/comp308-w2019-lesson10a",
+//"secret": "SomeSecret"
+"URI" : "mongodb://parupatel:Paru12345@ds155634.mlab.com:55634/comp308-assignment2"
 }
